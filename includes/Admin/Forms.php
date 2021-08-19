4604eed7bd30f350330972b4b5bc190bd30dee75
ok9xnirab <istiaqanirab202@gmail.com>
2021-08-19 20:53:51 +0600

    Add #4

--- includes/Admin/Forms.php
+++ includes/Admin/Forms.php
@@ -27,10 +27,12 @@ class Forms
             $display_product_share_button = sanitize_text_field($_POST['display_product_share_button']);
             $buttons_visible_position = sanitize_text_field($_POST['buttons_visible_position']);
             $button_design = sanitize_text_field($_POST['button_design']);
+            $blank_tab_enable = isset($_POST['blank_tab_enable']);
 
             update_option('psb_display_product_share_button', $display_product_share_button);
             update_option('psb_buttons_visible_position', $buttons_visible_position);
             update_option('psb_button_design', $button_design);
+            update_option('blank_tab_enable', $blank_tab_enable);
 
             if (is_array($_POST['social_buttons'])) {
                 update_option('psb_social_buttons', $this->sanitize_array_field($_POST['social_buttons']));
